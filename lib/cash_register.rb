dd704b1fb907b6394f9536de72dbcc9659df6109
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-10 02:11:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction
+  attr_accessor :total, :discount#, :last_transaction
   
   def initialize(discount = 0)
     @total = 0
@@ -38,8 +38,8 @@ class CashRegister
   end
 
   def void_last_transaction
-    @last_transaction = items.length-1
-    @total = @total - @last_transaction
+    last_transaction = items.length-1
+    @total = @total - last_transaction
   end
 
 end
